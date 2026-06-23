69b28abca35160db419be8c9b1abc811dfd533b7
Taylor Reece <taylor.reece@prismatic.io>
2026-06-23 16:36:40 -0500

    Rename documentation.md to README.md

--- salesforce-initial-data-sync/src/index.ts
+++ salesforce-initial-data-sync/src/index.ts
@@ -11,7 +11,7 @@ import { integration } from "@prismatic-io/spectral";
 import flows from "./flows";
 import { configPages } from "./configPages";
 import { componentRegistry } from "./componentRegistry";
-import documentation from "../documentation.md";
+import documentation from "../README.md";
 
 export { configPages } from "./configPages";
 export { componentRegistry } from "./componentRegistry";
--- simple-initial-data-sync/src/index.ts
+++ simple-initial-data-sync/src/index.ts
@@ -2,7 +2,7 @@ import { integration } from "@prismatic-io/spectral";
 import flows from "./flows";
 import { configPages } from "./configPages";
 import { componentRegistry } from "./componentRegistry";
-import documentation from "../documentation.md";
+import documentation from "../README.md";
 
 export { configPages } from "./configPages";
 export { componentRegistry } from "./componentRegistry";
